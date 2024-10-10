532685f2200bc919e41702a1a8745428a4340fd4
Mustafa Merchant <74061610+BlueStreak79@users.noreply.github.com>
2024-10-10 08:53:41 +0530

    Update host.bat

--- host.bat
+++ host.bat
@@ -1,5 +1,16 @@
 @echo off
-:: CorelDRAW and Adobe Host Blocker Script
+:: CorelDRAW and Adobe Host Blocker Script with PowerShell TLS 1.2 and Admin Privileges
+
+:: Check for administrative permissions
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%0' -ArgumentList '%*' -Verb RunAs"
+    exit /b
+)
+
+:: Ensure PowerShell stops on errors and enable TLS 1.2 for compatibility
+powershell -Command "$ErrorActionPreference = 'Stop'; [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12"
 
 echo Blocking CorelDRAW and Adobe domains...
 
@@ -32,17 +43,13 @@ echo 127.0.0.1 activate.wip3.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 na2m-pr.licenses.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 hlrcv.stage.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 lmlicenses.wip4.adobe.com >> %windir%\System32\drivers\etc\hosts
-echo 127.0.0.1 na2m-pr.licenses.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 ims-na1.adobelogin.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 ccmdls.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 swupmf.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 swupdl.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 prod-rel-ffc-ccm.oobesaas.adobe.com >> %windir%\System32\drivers\etc\hosts
-echo 127.0.0.1 na2m-pr.licenses.adobe.com >> %windir%\System32\drivers\etc\hosts
-echo 127.0.0.1 hlrcv.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 lm.licenses.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 na1r.services.adobe.com >> %windir%\System32\drivers\etc\hosts
-echo 127.0.0.1 hlrcv.stage.adobe.com >> %windir%\System32\drivers\etc\hosts
 echo 127.0.0.1 oobesaas.adobe.com >> %windir%\System32\drivers\etc\hosts
 
 :: Flush DNS cache
